991919aed2a42e53dbc883de2843dcae5d73398f
richard <richard@vbox.myguest.virtualbox.org>
2025-08-04 19:09:45 +0300

    added file test.c

--- Steven_Prata/Chapter_4/task4.1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void){
-    char name[10];
-    char surname[10];
-    scanf("%s", &name);
-    scanf("%s", &surname);
-    printf("%s %s", surname, name);
-    return 0;
-}
