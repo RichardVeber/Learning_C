a5f32b8a241b72d558fc70bd10619090dec8b94e
richard <richard.veber2010@gmail.com>
2025-08-09 11:54:57 +0300

    added text to task4.8.c

--- Steven_Prata/Chapter_4/task4.8.c
+++ Steven_Prata/Chapter_4/task4.8.c
@@ -4,14 +4,16 @@ int main(void){
     const float coefficient_1 = 3.785;
     const float coefficient_2 = 1.609;
     float miles,gallons;
+    printf("Введите количество преодоленных миль:");
     scanf("%f", &miles);
+    printf("Введите количество израсходованных галлонов бензина:");
     scanf("%f", &gallons);
     float miles_per_gallon = miles/gallons;
     float gallons_per_mile = gallons/miles;
     float liters = gallons*coefficient_1;
     float kilometers  = miles*coefficient_2;
     float liters_per_100_kilometers = liters/kilometers*100;
-    printf("%.1f\n",miles_per_gallon);
-    printf("%f  ",liters_per_100_kilometers);
+    printf("\nКоличество миль, пройденных на одном галлоне - %.1f\n",miles_per_gallon);
+    printf("Ваше потребление горючего составляет %.1f литров на 100км.\n",liters_per_100_kilometers);
     return 0;
-}                                    
+}
